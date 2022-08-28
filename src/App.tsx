6aa1ec5b9dc94c78127868149c772fe08ece9498
Daniel <yunlew531@gmail.com>
2022-08-28 20:40:08 +0800

    [ADD] 配置 eslint airbnb

--- src/App.tsx
+++ src/App.tsx
@@ -5,31 +5,33 @@ import type { Theme } from 'styleSheets/theme';
 import styled from '@emotion/styled';
 
 const H1 = styled.h1<{ theme?: Theme }>`
-  font-size: ${({ theme: { fontSizes: { fs_1 } }}) => fs_1};
-  color: ${({ theme: { colors: { secondary } }}) => secondary};
+  font-size: ${({ theme: { fontSizes: { fs_1 } } }) => fs_1};
+  color: ${({ theme: { colors: { primary } } }) => primary};
 `;
 
-function App() {
-  return (
-    <ThemeProvider theme={theme}>
-      <div className="App">
-        <header className="App-header">
-          <H1>this is h1</H1>
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    </ThemeProvider>
-  );
-}
+const App = () => (
+  <ThemeProvider theme={theme}>
+    <div className="App">
+      <header className="App-header">
+        <H1>this is h1</H1>
+        <p>
+          Edit
+          {' '}
+          <code>src/App.tsx</code>
+          {' '}
+          and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  </ThemeProvider>
+);
 
 export default App;
