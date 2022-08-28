99750f1d2faf61fcf4be2641b79317d75f019b47
Daniel <yunlew531@gmail.com>
2022-08-28 19:41:01 +0800

    [ADD] 配置 style-components 樣式

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,34 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { ThemeProvider } from '@emotion/react';
+import { theme } from 'styleSheets/theme';
+import type { Theme } from 'styleSheets/theme';
+import styled from '@emotion/styled';
+
+const H1 = styled.h1<{ theme?: Theme }>`
+  font-size: ${({ theme: { fontSizes: { fs_1 } }}) => fs_1};
+  color: ${({ theme: { colors: { secondary } }}) => secondary};
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <div className="App">
+        <header className="App-header">
+          <H1>this is h1</H1>
+          <p>
+            Edit <code>src/App.tsx</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    </ThemeProvider>
   );
 }
 
---
+++ src/styleSheets/theme.ts
@@ -0,0 +1,28 @@
+export const theme = {
+    colors: {
+        primary: '#355F8B',
+        secondary: '#D08181',
+        blue: '#3591C5',
+        green: '#7FC0C5',
+        teal: '#5E9BAE',
+        darkGreen: '#52797C',
+        darkBlue: '#283C43',
+        gray_900: '#000000',
+        gray_700: '#444444',
+        gray_600: '#888888',
+        gray_500: '#D5D5D5',
+        gray_300: '#EEEEEE',
+        gray_100: '#F5F6F6',
+        white: '#FFFFFF',
+    },
+    fontSizes: {
+        fs_1: '20px',
+        fs_2: '18px',
+        fs_3: '16px',
+        fs_4: '15px',
+        fs_5: '14px',
+        fs_6: '12px',
+    },
+  };
+  
+  export type Theme = typeof theme;
